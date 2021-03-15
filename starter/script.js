eeeda8d1eef6fc896b14bd01ae6104f948167997
attobra <nora.attobra@gmail.com>
2021-03-15 18:56:10 -0400

    proto

--- starter/script.js
+++ starter/script.js
@@ -22,3 +22,28 @@ const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 
 console.log(matilda, jack);
+console.log(jonas instanceof Person); //true
+
+//prototype of the linked object
+//not prototype of person, but prototye of matilda or jonas
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__ === Person.prototype); //true
+
+console.log(Person.prototype.isPrototypeOf(jonas)); //true
+console.log(Person.prototype.isPrototypeOf(matilda)); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //FALSE
+
+// you can add some properties in the prototype but not in the object itself
+Person.prototype.species = 'homo Sapiens';
+console.log(jonas.species);
+
+console.log(jonas.hasOwnProperty('firstName')); //true
+
+console.log(jonas.hasOwnProperty('species')); //FALSE
